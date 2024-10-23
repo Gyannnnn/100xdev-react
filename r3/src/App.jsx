557c2f48047bc2eb054999a1f416b56675b8f0dc
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-23 16:04:41 +0530

    => ROUTING IN REACT

--- r3/src/App.jsx
+++ r3/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link  , useNavigate} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+
+      <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/class11">class11</Link>
+      <Link to="/class12">class12</Link>
+        <Routes>
+          <Route path="/class11" element={<Class11/>} />
+          <Route path="/class12" element={<Class12/>}/>
+          <Route path="/" element={<Home/>} />
+          <Route path="*" element={<Err/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+function Class11(){
+  return <div>
+    Welcome to class11
+  </div>
+}
+
+function Class12(){
+
+const navigate = useNavigate()
+
+function redirectUser(){
+  navigate("/")
+}
+
+
+  return <div>
+    Welcome to class 12
+    <button onClick={redirectUser}>Go Back To The Home Page</button>
+  </div>
+}
+
+function Home(){
+  return <div>
+    Home page
+  </div>
+}
+
+function  Err(){
+  return <div>
+    Sorry page Not Found !
+  </div>
+}
