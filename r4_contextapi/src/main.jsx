6ce8b37e44146c10043bd8a2b61734938b572281
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-24 20:13:10 +0530

    => Recoil

--- r4_contextapi/src/main.jsx
+++ r4_contextapi/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ContextApi from './ContextApi.jsx'
-
+import Recoil from './Recoil.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <ContextApi/>
+    {/* <ContextApi/> */}
+    <Recoil/>
   </StrictMode>,
 )
